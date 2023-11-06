9ff6a12cee5e67b5acc9ce4372a341c97efde32a
Shin <shinoku1801@gmail.com>
2023-11-06 10:54:23 +0700

    add some dummy datas

--- client/src/app/api/category/route.ts
+++ client/src/app/api/category/route.ts
@@ -2,9 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        const response = await fetch("http://localhost:8080/api/category");
-        const data = await response.json();
-        return NextResponse.json(data);
+        // const response = await fetch("http://localhost:8080/api/category");
+        // const data = await response.json();
+        return NextResponse.json([
+            {
+                _id: "6517c4a03486c3578a5b7b9b",
+                name: "Dreamcatcher",
+                slug: "dreamcatcher",
+            },
+            {
+                _id: "6517c4a03486c3578a5b7b9c",
+                name: "Macrame",
+                slug: "macrame",
+            },
+        ]);
     } catch (error) {
         return NextResponse.json(error);
     }
