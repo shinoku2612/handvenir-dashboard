961332ecefef3646ec6b556992ef1334557577f9
vanthong-iSoft <vanthong@i-soft.com.vn>
2023-12-06 15:38:52 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "**",
+                hostname: "drive.google.com",
                 port: "",
-                pathname: "/**",
+                pathname: "/drive/**",
             },
         ],
     },
