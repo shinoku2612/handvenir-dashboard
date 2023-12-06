dcd9cf1936720bc21e7c4a506250226a15871638
vanthong-iSoft <vanthong@i-soft.com.vn>
2023-12-06 15:33:42 +0700

    update

--- next.config.js
+++ next.config.js
@@ -8,12 +8,6 @@ const nextConfig = {
                 port: "",
                 pathname: "/**",
             },
-            {
-                protocol: "https",
-                hostname: "drive.google.com",
-                port: "",
-                pathname: "/**",
-            },
         ],
     },
     trailingSlash: false,
