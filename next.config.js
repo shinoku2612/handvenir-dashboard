47fea61e972d0766222d058654ac408c9c2fbfc1
vanthong-iSoft <vanthong@i-soft.com.vn>
2023-12-06 16:03:17 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "**",
+                port: "*",
                 pathname: "/**",
             },
         ],
