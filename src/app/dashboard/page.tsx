0f7c136e45ff6b876f5e7eecf1ffe36f79215a64
vanthong-iSoft <vanthong@i-soft.com.vn>
2023-12-08 18:50:32 +0700

    track production error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,6 +22,7 @@ export default async function DashBoard() {
             getSaleCount(),
             getEarningCount(),
         ]);
+    console.log(earning, typeof earning);
     return (
         <div>
             <div className="flex flex-wrap lg:flex-nowrap justify-center">
@@ -35,7 +36,7 @@ export default async function DashBoard() {
                         <div className="">
                             <p className="font-bold text-gray-400">Earnings</p>
                             <p className="text-2xl text-shadow-sm">
-                                ${0}
+                                ${earning}
                             </p>
                         </div>
                     </div>
